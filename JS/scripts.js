4bd496c7ff51ffe09e1b736cac15de7783bb1b41
Horacio Ontiveros <34705845+HoracioOn@users.noreply.github.com>
2018-04-25 18:58:03 -0500

    mas ajustes visuales
    Corregí los colores del banner principal en el JS, algunos experimentos con el divisor entre el test y el area de clientes, cambié el color de la selección

--- JS/scripts.js
+++ JS/scripts.js
@@ -97,15 +97,14 @@ var c1_0 = colors[colorIndices[2]];
 var c1_1 = colors[colorIndices[3]];
 
 var istep = 1 - step;
-var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+
 var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
 var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-var color1 = "rgb("+r1+","+g1+","+b1+")";
+var color1 = "rgb("+0+","+g1+","+b1+")";
 
-var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
 var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
 var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-var color2 = "rgb("+r2+","+g2+","+b2+")";
+var color2 = "rgb("+0+","+g2+","+b2+")";
 
  $('#gradient').css({
    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
